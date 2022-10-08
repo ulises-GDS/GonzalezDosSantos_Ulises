bbc7765a8f04b553fc25db6f98c9ac0c0126a02c
Ulises González Dos Santos <115324215+ulises-GDS@users.noreply.github.com>
2022-10-07 23:17:45 -0300

    Add files via upload
    Adjunto archivos formato .html y .js al repositorio.
    
    Added the .html and .js files  to the repository.

---
+++ modulo01/tp1/Ejercicio6.js
@@ -0,0 +1,5 @@
+//Se creo el archivo con extension .js para poder desarrollar el Array solicitado.
+var dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes",
+"Sabado",
+"Domingo"]
+console.log(dias);
